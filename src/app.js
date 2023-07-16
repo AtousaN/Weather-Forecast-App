4db667a68e80ac281b69ad6b2908b98f9bf28c4e
Atousa <at_nt.1995@yahoo.com>
2023-07-17 01:14:05 +0330

    Create forecast loop

--- src/app.js
+++ src/app.js
@@ -13,6 +13,30 @@ function formatDate(timestamp) {
     return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+    let forecastElement = document.querySelector("#forecast");
+    let forecastHTML = `<div class="row">`;
+    let days = ["Thu", "Fri", "Sat", "Sun"];
+    days.forEach(function (day) {
+        forecastHTML = forecastHTML + `
+        <div class="col-2">
+            <div class="weather-forecast-date">
+                ${day}
+            </div>
+            <img src="https://openweathermap.org/img/wn/50d@2x.png" alt="" width="42" />
+            <div class="weather-forecast-temperatures">
+                <span class="weather-forecast-temperatures-max">
+                    18º </span>
+                <span class="weather-forecast-temperatures-min">
+                    12º </span>
+            </div>
+        </div>`;
+    })
+    
+    forecastHTML = forecastHTML + `</div>`;
+    forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
     let temperatureElement = document.querySelector("#temperature");
     let cityElement = document.querySelector("#city");
@@ -75,3 +99,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("New York");
+displayForecast();
