0efc41948ae49457cc784347ded39247d9e60256
Atousa <at_nt.1995@yahoo.com>
2023-07-17 10:42:35 +0330

    Get coordinates of the searched city

--- src/app.js
+++ src/app.js
@@ -13,10 +13,11 @@ function formatDate(timestamp) {
     return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
+    console.log(response.data);
     let forecastElement = document.querySelector("#forecast");
-    let forecastHTML = `<div class="row">`;
     let days = ["Thu", "Fri", "Sat", "Sun"];
+    let forecastHTML = `<div class="row">`;
     days.forEach(function (day) {
         forecastHTML = forecastHTML + `
         <div class="col-2">
@@ -37,6 +38,14 @@ function displayForecast() {
     forecastElement.innerHTML = forecastHTML;
 }
 
+function getForecast(coordinates) {
+    console.log(coordinates);
+    let apiKey = "255d1980e372838f0f0a6d06249ddfb8";
+    let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+    console.log(apiUrl);
+    axios.get(apiUrl).then(displayForecast);
+}
+
 function displayTemperature(response) {
     let temperatureElement = document.querySelector("#temperature");
     let cityElement = document.querySelector("#city");
@@ -55,7 +64,9 @@ function displayTemperature(response) {
     windElement.innerHTML = Math.round(response.data.wind.speed * 3.6);
     dateElement.innerHTML = formatDate(response.data.dt * 1000);
     iconElement.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
-    iconElement.setAttribute ("alt", response.data.weather[0].description);
+    iconElement.setAttribute("alt", response.data.weather[0].description);
+    
+    getForecast(response.data.coord);
 }
 
 function search(city) {
